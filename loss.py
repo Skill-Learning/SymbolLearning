b6197c042d4a08aa869d0d5552fd439eb286b03a
Manpreet Singh <manprees@andrew.cmu.edu>
2022-11-30 17:44:52 -0500

    loss function done

--- loss.py
+++ loss.py
@@ -22,30 +22,32 @@ class SupConLoss(nn.Module):
         self.contrast_mode = contrast_mode
         self.base_temperature = base_temperature
 
-    def forward(self, features, labels=None, mask=None):
+    def forward(self, embeddings, pose_change, labels=None, mask=None):
         """Compute loss for model. If both `labels` and `mask` are None,
-        it degenerates to SimCLR unsupervised loss:
-        https://arxiv.org/pdf/2002.05709.pdf
-
         Args:
-            features: hidden vector of shape [bsz, n_views, ...].
-            labels: ground truth of shape [bsz].
+            embeddings: img+ data [B, embed_size]
+            labels: [B,] : label_class [action_vector + object type one hot vector -> one single class]
+            pose_change: [B, delta_pose]
             mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
                 has the same class as sample i. Can be asymmetric.
         Returns:
             A loss scalar.
         """
         device = (torch.device('cuda')
-                  if features.is_cuda
+                  if embeddings.is_cuda
                   else torch.device('cpu'))
 
-        if len(features.shape) < 3:
-            raise ValueError('`features` needs to be [bsz, n_views, ...],'
-                             'at least 3 dimensions are required')
-        if len(features.shape) > 3:
-            features = features.view(features.shape[0], features.shape[1], -1)
+        batch_size = embeddings.shape[0]
+        embeddings = embeddings.view(batch_size, 1,-1)
+        embed_size = embeddings.shape[-1]
 
-        batch_size = features.shape[0]
+        if pose_change.shape[0]!= batch_size:
+            raise ValueError('Number of poses do not match the batch size')
+
+        if labels.shape[0]!= batch_size:
+            raise ValueError('Number of labels do not match the batch size')
+
+
         if labels is not None and mask is not None:
             raise ValueError('Cannot define both `labels` and `mask`')
         elif labels is None and mask is None:
@@ -58,10 +60,10 @@ class SupConLoss(nn.Module):
         else:
             mask = mask.float().to(device)
 
-        contrast_count = features.shape[1]
-        contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
+        contrast_count = embeddings.shape[1]
+        contrast_feature = torch.cat(torch.unbind(embeddings, dim=1), dim=0)
         if self.contrast_mode == 'one':
-            anchor_feature = features[:, 0]
+            anchor_feature = embeddings[:, 0]
             anchor_count = 1
         elif self.contrast_mode == 'all':
             anchor_feature = contrast_feature
@@ -90,6 +92,18 @@ class SupConLoss(nn.Module):
 
         # compute log_prob
         exp_logits = torch.exp(logits) * logits_mask
+        # TODO: multiply the exp_logits with the pose change
+        # convert to rotattion matrix R
+        # del_r = acos(tr(Ri, R_n)-1/2)
+        #  rot_contrast_dot = rot@rot.T
+        # mask: all positives are 1, negatives are 0
+        # logits_mask: all are 1 except anchor
+        # mask_neg = ones - mask
+        # rot_contrast_dot = rot_contrast_dot * mask_neg
+        # del_r = torch.acos(tr(rot_contrast_dot) - 1)/2
+        # del_r = del_r/torch.pi
+        # exp_logits = exp_logits * del_r
+
         log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
 
         # compute mean of log-likelihood over positive
